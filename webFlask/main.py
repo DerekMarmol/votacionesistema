86c9a8131f87c27b6aa8a4f1eb314f41af22a4bc
DerekMarmol <134981434+DerekMarmol@users.noreply.github.com>
2024-04-15 13:27:05 -0600

    Add files via upload

--- webFlask/main.py
+++ webFlask/main.py
@@ -1,13 +1,8 @@
-from flask import Flask, render_template, request, jsonify, Response, session
-import cv2
-import face_recognition
+from flask import Flask, render_template, request, jsonify, session
 import numpy as np
-import requests
-from io import BytesIO
 import pandas as pd
 import secrets
 import logging
-from collections import Counter
 from flask_socketio import SocketIO, emit
 import os
 
@@ -19,26 +14,10 @@ app = Flask(__name__, static_folder=static_folder)
 app.static_folder = 'static'
 socketio = SocketIO(app)
 
-# Lista de imágenes de referencia y sus codificaciones
-url_imagenes_google_drive = [
-    'https://drive.google.com/uc?export=download&id=1ad6T_AlcyUmax_Qlw8ifvEOEARMJup-_',
-    'https://drive.google.com/uc?export=download&id=1SCxK7xp-MaI2lCW2AcQaR2JDfi0npeyz',
-    'https://drive.google.com/uc?export=download&id=1TF89ZanVI79Vk8QCromAwEZBKxqwHhjw'
-]
-codificaciones_existente = []
+def generar_token():
+    return secrets.token_hex(16)  # Genera un token hexadecimal de 16 bytes
 
-for url_imagen in url_imagenes_google_drive:
-    respuesta = requests.get(url_imagen)
-    imagen_existente = face_recognition.load_image_file(BytesIO(respuesta.content))
-    codificacion_existente = face_recognition.face_encodings(imagen_existente)[0]
-    codificaciones_existente.append(codificacion_existente)
-
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-@app.before_request
-def log_request_info():
-    app.logger.info('Headers: %s', request.headers)
-    app.logger.info('Body: %s', request.get_data())
+app.secret_key = '1309RTA2005'
 
 @app.route('/')
 def index():
@@ -49,35 +28,12 @@ def index():
 
 usuarios_df = pd.read_excel('base.xlsx')
 
-# Calcular y agregar las codificaciones de las imágenes de los usuarios
-codificaciones_usuarios = []
-for enlace_drive in usuarios_df['enlace_de_drive']:
-    respuesta = requests.get(enlace_drive)
-    imagen_usuario = face_recognition.load_image_file(BytesIO(respuesta.content))
-    codificacion_usuario = face_recognition.face_encodings(imagen_usuario)[0]
-    codificaciones_usuarios.append(codificacion_usuario)
-
-usuarios_df['codificacion'] = codificaciones_usuarios
-
-def generar_token():
-    return secrets.token_hex(16)  # Genera un token hexadecimal de 16 bytes
-
-app.secret_key = '1309RTA2005'
-
 @app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
-        imagen_enviada = request.files['imagen']
         email = request.form['email']
         password = request.form['password']
         
-        # Verificar reconocimiento facial
-        imagen_enviada = face_recognition.load_image_file(imagen_enviada)
-        codificaciones_enviadas = face_recognition.face_encodings(imagen_enviada)
-
-        if not codificaciones_enviadas:
-            return jsonify({'resultado': 'No se detectó rostro'})
-
         # Verificar correo electrónico y contraseña
         usuario = usuarios_df[(usuarios_df['correo'] == email) & (usuarios_df['contraseña'] == password)]
         if usuario.empty:
@@ -90,20 +46,12 @@ def login():
         # Almacenar el nombre del usuario en la sesión
         session['nombre'] = usuario.iloc[0]['nombre']
 
-        for enlace_drive in usuario['enlace_de_drive']:
-            respuesta = requests.get(enlace_drive)
-            imagen_usuario = face_recognition.load_image_file(BytesIO(respuesta.content))
-            codificacion_usuario = face_recognition.face_encodings(imagen_usuario)[0]
-            results = face_recognition.compare_faces(codificaciones_enviadas, codificacion_usuario)
-            if True in results:
-                token = secrets.token_hex(16)
-                session['correo'] = email
-                session['token'] = token
-                usuarios_df.at[usuario.index[0], 'token'] = token
-                usuarios_df.to_excel('base.xlsx', index=False)
-                return jsonify({'resultado': 'Coincide', 'token': token, 'enlace_de_drive': enlace_drive})
-        else:
-            return jsonify({'resultado': 'No coincide'})
+        token = secrets.token_hex(16)
+        session['correo'] = email
+        session['token'] = token
+        usuarios_df.at[usuario.index[0], 'token'] = token
+        usuarios_df.to_excel('base.xlsx', index=False)
+        return jsonify({'resultado': 'Coincide', 'token': token})
 
     return render_template('index.html', es_admin=session.get('es_admin', False))
 
@@ -216,37 +164,6 @@ def reiniciar_votaciones():
     usuarios_votaron = set()  # Vaciar el conjunto de usuarios que han votado
     reiniciar_votos()  # Reiniciar también los votos de los grupos
     return jsonify({'resultado': 'Votaciones reiniciadas correctamente'})
-
-def generate_frames():
-    cap = cv2.VideoCapture(0)
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
-        else:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-                face_detected = frame[y:y+h, x:x+w]
-                face_detected_rgb = cv2.cvtColor(face_detected, cv2.COLOR_BGR2RGB)
-                face_encodings = face_recognition.face_encodings(face_detected_rgb)
-                if face_encodings:
-                    for codificacion_existente in codificaciones_existente:
-                        results = face_recognition.compare_faces([codificacion_existente], face_encodings[0])
-                        if results[0]:
-                            cv2.putText(frame, "Coincide", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-                            break
-                    else:
-                        cv2.putText(frame, "No coincide", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @socketio.on('connect')
 def handle_connect():
